fa8e1ce7e599f8e4b3cb54f5e7119153e6c9e32e
町田秀行 <hideyuki.machida.jp@gmail.com>
2020-08-02 17:39:10 +0900

    update

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ProcessLogger.Swift",
+            type: .dynamic,
             targets: ["ProcessLogger.Swift"]),
     ],
     dependencies: [
